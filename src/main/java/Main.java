38cce500ff8843263aa215ffdb22f1ac512d8669
franMV4 <79657510+franMV4@users.noreply.github.com>
2021-06-17 19:50:06 +0200

    primera subida proyecto

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[]args){
+        Menu menu;
+        Restaurante restaurante;
+        Usuario usuario;
+
+        menu = new Menu("0", "0");
+        restaurante = new Restaurante("Jorge", "Argualas", "s", "h", "2");
+        usuario = new Usuario("Adrian", "74642478C", "g", "d");
+
+    }}
+
